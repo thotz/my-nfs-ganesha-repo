891a45af66d1f80e27eaa95cdd80bd4b5ccbe524
Jiffin Tony Thottan <jthottan@redhat.com>
2015-02-23 16:57:36 +0530

    Parsing the global options for FSAL_GLUSTER
    	* The global configurable options such as maxread are stored in (glusterfs_fsal_module)->fs_info.
    	  Right now we are setting this values as default and not reading the values mentioned inside
    	  configuration file.
    	* In this patch , a block named "GLUSTER" is introduced in the configuartion file where user can
    	  define the global options.The advantage is that instead of parsing every block in the conf file,
    	  only this block need to be parsed in which all types of global option can be included.
    	* Here only eight options are included link_support, symlink_support, acl_support, umask, pnfs_mds,
    	  pnfs_ds, auth_xdev_export, xattr_access_rights.The number can be increased according to the user's
    	  requirement
    BUG: 1195205
    Signed-off-by: Jiffin Tony Thottan <jthottan@redhat.com>

--- src/FSAL/FSAL_GLUSTER/main.c
+++ src/FSAL/FSAL_GLUSTER/main.c
@@ -69,8 +69,58 @@ static struct fsal_staticfsinfo_t default_gluster_info = {
 	.xattr_access_rights = 0400,	/* root=RW, owner=R */
 };
 
-static struct glusterfs_fsal_module *glfsal_module;
 
+static struct config_item glfs_params[] = {
+	CONF_ITEM_BOOL("link_support", true,
+		       fsal_staticfsinfo_t, link_support),
+	CONF_ITEM_BOOL("symlink_support", true,
+		       fsal_staticfsinfo_t, symlink_support),
+	CONF_ITEM_MODE("acl_support", FSAL_ACLSUPPORT_ALLOW|FSAL_ACLSUPPORT_DENY,
+		       fsal_staticfsinfo_t, acl_support),
+	CONF_ITEM_MODE("umask", 0,
+		       fsal_staticfsinfo_t, umask),
+	CONF_ITEM_BOOL("auth_xdev_export", false,
+		       fsal_staticfsinfo_t, auth_exportpath_xdev),
+	CONF_ITEM_MODE("xattr_access_rights", 0400,
+		       fsal_staticfsinfo_t, xattr_access_rights),
+	CONF_ITEM_BOOL("pnfs_mds", false,
+		       fsal_staticfsinfo_t, pnfs_mds),
+	CONF_ITEM_BOOL("pnfs_ds", false,
+		       fsal_staticfsinfo_t, pnfs_ds),
+	CONFIG_EOL
+};
+
+struct config_block glfs_param = {
+	.dbus_interface_name = "org.ganesha.nfsd.config.fsal.gluster",
+	.blk_desc.name = "GLUSTER",
+	.blk_desc.type = CONFIG_BLOCK,
+	.blk_desc.u.blk.init = noop_conf_init,
+	.blk_desc.u.blk.params = glfs_params,
+	.blk_desc.u.blk.commit = noop_conf_commit
+};
+
+static fsal_status_t init_config(struct fsal_module *fsal_hdl,
+				 config_file_t config_struct,
+				 struct config_error_type *err_type)
+{
+	struct glusterfs_fsal_module *glfsal_module =
+	    container_of(fsal_hdl, struct glusterfs_fsal_module, fsal);
+
+
+	glfsal_module->fs_info = default_gluster_info;
+	(void) load_config_from_parse(config_struct,
+				      &glfs_param,
+				      &glfsal_module->fs_info,
+				      true,
+				      err_type);
+	if (!config_error_is_harmless(err_type))
+		return fsalstat(ERR_FSAL_INVAL, 0);
+	display_fsinfo(&glfsal_module->fs_info);
+
+	return fsalstat(ERR_FSAL_NO_ERROR, 0);
+}
+
+static struct glusterfs_fsal_module *glfsal_module;
 /* Module methods
  */
 
@@ -95,8 +145,7 @@ MODULE_INIT void glusterfs_init(void)
 	glfsal_module->fsal.m_ops.create_export = glusterfs_create_export;
 
 	/* setup global handle internals */
-	glfsal_module->fs_info = default_gluster_info;
-
+	glfsal_module->fsal.m_ops.init_config = init_config;
 	LogDebug(COMPONENT_FSAL, "FSAL Gluster initialized");
 }
 
